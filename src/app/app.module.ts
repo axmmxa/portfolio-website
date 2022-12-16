54efdc25aee1654c7a22c60527c4bf2adde7e069
Alexander Much <46798654+axmmxa@users.noreply.github.com>
2022-12-16 15:48:31 +0100

    about-me componet created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
 import { MySkillsComponent } from './my-skills/my-skills.component';
 import { MyWorkComponent } from './my-work/my-work.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { MyWorkComponent } from './my-work/my-work.component';
     HeaderComponent,
     MainComponent,
     MySkillsComponent,
-    MyWorkComponent
+    MyWorkComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
